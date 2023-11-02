dc03432b4b3d7efcad1d112535bcd5eccbc8b4a7
Jack <jconnolly444@gmail.com>
2023-11-02 13:02:10 -0500

    installed webpack, npm and Babel. linked pages and ran various commands in index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+var moment = require("moment");
+
 console.log("Hello from Javascript!");
+
+var datetime = moment().startOf("day").fromNow();
+console.log(datetime);
+
+var ftime = moment("20120620", "YYYYMMDD").fromNow();
+console.log(ftime);
